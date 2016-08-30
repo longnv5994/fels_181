9e4ae32d30dbfea66d01a96a3d2ffe1140e81589
longnv5994 <longnv5994@gmail.com>
2016-08-30 09:22:02 +0700

    Edit profile

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem "bcrypt", "~> 3.1.7"
 
 gem "bootstrap-sass", "~> 3.3.1"
 
+gem "config"
 # Use Unicorn as the app server
 # gem "unicorn"
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,34 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
+  end
+
+  def show
+  end
+
+  def index
+    @user = User.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "update_profile"
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   def create
     @user = User.new user_params
     if @user.save
-      redirect_to user_path
+      flash[:success] = t "create"
+      redirect_to @user
     else
       render :new
     end
@@ -16,5 +38,13 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit :name, :email, :password,
         :password_confirmation
+    end
+
+    def load_user
+      @user = User.find_by id: params[:id]
+      if @user.nil?
+        flash[:danger] = t "miss_user"
+        redirect_to root_url
+      end
     end
 end
