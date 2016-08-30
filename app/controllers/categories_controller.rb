214166803688b8f55cd031f153830efa9af9b7b0
Nguyen Huong <nguyen.thi.thu.huong@framgia.com>
2016-08-30 19:26:01 +0700

    Signup, signin, remember user

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,50 @@
+class CategoriesController < ApplicationController
+  before_action :logged_in?, only: [:create, :destroy]
+  before_action :load_category, only: [:edit, :update, :destroy]
+
+  def index
+    @categories = Category.all
+    @category = Category.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update_attributes category_params  
+      flash[:success] = t "update_success"
+    end
+      redirect_to categories_path
+  end  
+
+  def create
+    @category = Category.new category_params
+      if @category.save
+        flash[:success] = t "add_success"
+      end
+    redirect_to :back
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to :back
+  end
+
+  def show
+    @categories = Category.all
+    @category = Category.new  
+  end
+
+  private
+  def category_params
+    params.require :category.permit(:name)
+  end
+
+  def load_category
+    @category = Category.find_by id: params[:id]
+    if @category.nil?
+      flash[:danger] = t "category.blank"
+      redirect_to root_url
+    end
+  end
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :lessons
   has_many :words
-
+  default_scope {order(created_at: :desc)}
   validates :name, presence: true, length: {maximum: 255}
 end
