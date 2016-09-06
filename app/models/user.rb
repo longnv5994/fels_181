793544531cfcde8b94aea81bb3f91c47c0e47f0f
longnv5994 <longnv5994@gmail.com>
2016-09-06 18:37:09 +0700

    Do lesson

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,8 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
+  accepts_nested_attributes_for :lessons
+
   def User.digest string
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
     BCrypt::Engine.cost
