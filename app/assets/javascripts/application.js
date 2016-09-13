2fe171ef7f1d134498ffd04e2982d14e3b09145b
Khang <anhkhang2304@gmail.com>
2016-09-13 16:22:18 +0700

    Fix bug filter word, css, and js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,7 +35,8 @@ function remove_fields(link) {
 $(document).on('ready page:load', function() {
   loadPageBody($('#grid-content'),$('#btnsearch').data('url'));
   $('.grid-word').on('click','#btnsearch',function(){
-    loadPageBody($('#grid-content'),$(this).data('url'));
+    var dataseri = $('form').serialize();
+    loadPageBody($('#grid-content'),$(this).data('url'),dataseri);
   })
   $('.txtkeyword').keypress(function(e) {
     if(e.which == 13) {
@@ -66,22 +67,94 @@ $(document).on('ready page:load', function() {
     });
     e.preventDefault();
   });
+
+  $('.grid-word').on('click','ul.pagination li a', function(e){
+    var dataseri = $('form').serialize();
+    dataseri = dataseri + "&page=" + $(this).text();
+    loadPageBody($('#grid-content'), $('#btnsearch').data('url'), dataseri);
+    e.preventDefault();
+  })
+
+  $(document).on('click', 'a.act-view', function (event) {
+    var $grid = $(this).closest("div.grid-data");
+    var datapost = $grid.data("parameters");
+    if (datapost == 'undefined') {
+      datapost = {}
+    }
+    var dataFinish = {};
+    $.extend(dataFinish, datapost);
+    openDialogView('Info', $(this).data('view'), datapost, $grid.data("size"));
+    event.stopPropagation();
+  });
 })
 
 function loadPageBody($container, url, data) {
   try {
     var dataseri = $('form').serialize();
+    if(data == undefined)
+      data = dataseri;
     $container.html("<img src='/assets/images/loading.gif' />");
     var dfd = $.Deferred();
-    $container.load(url, dataseri, function (response, status, xhr) {
+    $container.load(url, data, function (response, status, xhr) {
       if (status == 'error') {
         var msg = I18n.t("status.errors") + xhr.status + " " + xhr.statusText;
         $container.html(msg);
       }
+      $('.grid-word ul.pagination li.prev,.grid-word ul.pagination li.next').css("display","none");
       return dfd.resolve();
     });
     return dfd.promise();
   } catch (e) {
     $container.html(e.message);
   }
+}
+
+function openDialogView(stitle, urlpageLoad, data, size) {
+  try {
+    var $container = $("<div></div>");
+    loadPage($container, urlpageLoad, data).done(function () {
+      var $dialog = BootstrapDialog.show({
+        title: stitle,
+        onshown: function (dialogRef) {
+        },
+        message: $container,
+        closable: true,
+        closeByBackdrop: false,
+        draggable: true,
+        buttons: [
+        {
+          label: I18n.t("exit"),
+          action: function (dialogRef) {
+            dialogRef.close();
+          }
+        }],
+      });
+
+      if (size == 'NORMAL')
+        $dialog.setSize(BootstrapDialog.SIZE_NORMAL);
+      else if (size == 'SMALL')
+        $dialog.setSize(BootstrapDialog.SIZE_SMALL);
+      else if (size == 'LARGE')
+        $dialog.setSize(BootstrapDialog.SIZE_LARGE);
+      else if (size == 'FULL') {
+        $dialog.setSize(BootstrapDialog.SIZE_FULL);
+      }
+      else
+        $dialog.setSize(BootstrapDialog.SIZE_WIDE);
+      $dialog.open();
+    })
+  } catch (e) {}
+}
+
+function loadPage($container, url, data) {
+  $container.html("<img src='/assets/images/loading.gif' />");
+  var dfd = $.Deferred();
+  $container.load(url, data, function (response, status, xhr) {
+    if (status == 'error') {
+      var msg = I18n.t('status.errors ') + xhr.status + " " + xhr.statusText;
+      $container.html(msg);
+    }
+    return dfd.resolve();
+  });
+  return dfd.promise();
 }
